b1e6ef1d0376418edfceb0bc3bec9e7feded270d
GJK2018 <847731382@qq.com>
2018-08-19 15:38:40 +0800

    01

--- Scripts/test01.py
+++ Scripts/test01.py
@@ -6,6 +6,10 @@ class Test01():
     @pytest.mark.parametrize("a", [1, 2, 3])
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step('我是测试步骤001')
-    def test_al(self, a):
+    def test01(self, a):
         allure.attach('描述', '我是测试步骤001的描述～～～')
         assert a != 2
+
+    def test02(self, a):
+        allure.attach('描述', '我是测试步骤002的描述～～～')
+        assert 1
